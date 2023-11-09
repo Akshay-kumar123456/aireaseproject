c6b6c92f483152b442b95ff1014ba5f35b5d4803
Akshay-kumar123456 <potnuru.akshaykumar@gmail.com>
2023-11-09 17:39:52 +0530

    initialcommit

---
+++ airlinereservationsystem/src/main/java/com/airlinereservationsystem/main/AirlinereservationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.airlinereservationsystem.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinereservationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinereservationsystemApplication.class, args);
+	}
+
+}
---
+++ airlinereservationsystem/src/test/java/com/airlinereservationsystem/main/AirlinereservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlinereservationsystem.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinereservationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
