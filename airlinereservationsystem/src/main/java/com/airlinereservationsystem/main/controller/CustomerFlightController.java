475bf9fea84bc3962bd09099ebae47ef798028c6
tony1287256 <tony1287256@gmail.com>
2023-11-11 11:54:38 +0530

    Customer flight bookings

--- airlinereservationsystem/src/main/java/com/airlinereservationsystem/main/controller/CustomerFlightController.java
+++ airlinereservationsystem/src/main/java/com/airlinereservationsystem/main/controller/CustomerFlightController.java
@@ -1,14 +1,47 @@
 package com.airlinereservationsystem.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.airlinereservationsystem.main.exception.InvalidIDException;
+import com.airlinereservationsystem.main.model.Customer;
+import com.airlinereservationsystem.main.model.CustomerFlight;
+import com.airlinereservationsystem.main.model.Flight;
+import com.airlinereservationsystem.main.service.CustomerFlightService;
+import com.airlinereservationsystem.main.service.CustomerService;
+import com.airlinereservationsystem.main.service.FlightService;
 
 @RestController
 public class CustomerFlightController {
-	@PostMapping("/airease/book/{cid}/{fid}")
-	public void bookTicket(@PathVariable("cid")int cid ,@PathVariable("fid")int fid) {
+	@Autowired
+	private CustomerService customerService;
+	@Autowired
+	private FlightService flightService;
+
+	@Autowired
+	private CustomerFlightService customerFlightService;
+
+	@PostMapping("/flight/book/{cid}/{fid}")
+	public ResponseEntity<?> bookTicket(@PathVariable("cid") int cid, @PathVariable("fid") int fid,
+			@RequestBody CustomerFlight customerFlight) {
+
+		try {
+			Customer customer = customerService.getCustomer(cid);
+			Flight flight = flightService.getById(fid);
+			customerFlight.setCustomer(customer);
+			customerFlight.setFlight(flight);	
+			
+			return ResponseEntity.ok().body(customerFlight);
+
+		} catch (InvalidIDException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 		
+
 	}
 
 }
--- airlinereservationsystem/src/main/java/com/airlinereservationsystem/main/service/FlightService.java
+++ airlinereservationsystem/src/main/java/com/airlinereservationsystem/main/service/FlightService.java
@@ -2,11 +2,14 @@ package com.airlinereservationsystem.main.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.airlinereservationsystem.main.exception.InvalidIDException;
+import com.airlinereservationsystem.main.model.Customer;
 import com.airlinereservationsystem.main.model.Flight;
 import com.airlinereservationsystem.main.repository.FlightRepository;
 @Service
@@ -38,6 +41,15 @@ public class FlightService {
 	public List<Flight> getByDate(LocalDate date) {
 		
 		return flightRepository.getBydepartureDate(date);
+	}
+
+	public Flight getById(int fid) throws InvalidIDException {
+		Optional<Flight> optional = flightRepository.findById(fid);
+		if (!optional.isPresent()) {
+			throw new InvalidIDException("Customer ID invalid");
+		}
+		return optional.get();
+		
 	}
 
 	
